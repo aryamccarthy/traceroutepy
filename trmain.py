3355af8648b339f8000f58c2c3e3f026e821f604
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-20 19:11:04 -0600

    Added parser adapter.

--- trmain.py
+++ trmain.py
@@ -19,8 +19,37 @@ def get_route(ip_address):
 
   return output
 
+class RouteParser(object):
+  """
+  This class adapts the trparse package into a common interface.
+  """
+
+  def __init__(self, route):
+    super(RouteParser, self).__init__()
+    self._route = trp.loads(route)
+
+  def total_time(self):
+    """
+    Each route comprises multiple hops (each IP between the source and destination).
+    Each of those comprises three probes, each with their own duration.
+    With this, we add up the maximum route times to get a total path length.
+    IMPORTANT QUESTION: Should this be minimum?
+    """
+    # Uncomment for debugging.
+    # print [[probe.rtt for probe in hop.probes] for hop in self._route.hops]
+    return sum([max([probe.rtt for probe in hop.probes]) for hop in self._route.hops])
+
+  def all_stops(self):
+    pass
+    
+
+#def parse_route(route):
+#  duration = max([[[probe.rtt for probe in hop.probes] for hop in route.]])
+
 def main():
-  print trp.loads(get_route("8.8.8.8"))
+  route = get_route("8.8.8.8")
+  parser = RouteParser(route)
+  print parser.total_time()
 
 if __name__ == '__main__':
   main()
