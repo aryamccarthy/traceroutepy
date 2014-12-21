d792954aab25432b100e466edba9e159d17337c9
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-20 18:43:27 -0600

    Added trparse subproject

--- trmain.py
+++ trmain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
+import trparse.trparse as trp
 
 # @see http://softwaredevelopment.gr/1074/traceroute-system-call-in-python/
 def get_route(ip_address):
@@ -19,7 +20,7 @@ def get_route(ip_address):
   return output
 
 def main():
-  print get_route("8.8.8.8")
+  print trp.loads(get_route("8.8.8.8"))
 
 if __name__ == '__main__':
   main()
